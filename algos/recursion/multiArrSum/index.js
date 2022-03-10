154a6661bd8bc9c655bb00d265e77cf20b061dd2
Sharon Kindles <shalaskin29@gmail.com>
2022-03-10 14:43:41 -0600

    recursion

--- algos/recursion/multiArrSum/index.js
+++ algos/recursion/multiArrSum/index.js
@@ -1,5 +1,21 @@
 function multiDimSum(arr) {
- 
-}
+   
+        var sum = 0;
+        // iterate array using forEach, better to use for loop since it have higher performance
+        arr.forEach(function(v) {
+          // checking array element is an array
+          if (typeof v == 'object')
+            // if array then getting sum it's element (recursion)
+            sum += multiDimSum(v);
+          else
+            // else adding the value with sum
+            sum += v
+        })
+        // returning the result
+        return sum;
+      }
+      
+      console.log(multiDimSum([2, 5, [4, 6], 5]) === 22);
+
 
 module.exports = multiDimSum
