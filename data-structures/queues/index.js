e5b5688a4af877ad2c0ae52a897fee3548426481
Sharon Kindles <shalaskin29@gmail.com>
2022-03-15 16:10:44 -0500

    data-strutures

--- data-structures/queues/index.js
+++ data-structures/queues/index.js
@@ -1,8 +1,29 @@
 class Queue {
-	// Your code below!
-	constructor() {
-
-	}
+    constructor() {
+      this.elements = {};
+      this.head = 0;
+      this.size = 0;
+    }
+    enqueue(element) {
+      this.elements[this.size] = element;
+      this.size++;
+    }
+    dequeue() {
+      const item = this.elements[this.size];
+      delete this.elements[this.size];
+      this.size--;
+      return item;
+    }
+    peek() {
+      return this.elements[this.head];
+    }
+    get length() {
+      return this.size - this.head;
+    }
+    get isEmpty() {
+      return this.length === 0;
+    }
 }
+
 
 module.exports = Queue
--- data-structures/queues/index.test.js
+++ data-structures/queues/index.test.js
@@ -7,17 +7,17 @@ describe('Queue data structure', () => {
 		queue = new Queue()
 	})
 
-	xit('has a size of 0 initially', () => {
+	it('has a size of 0 initially', () => {
 		expect(queue.size).toBe(0)
 	})
 
 	describe('enqueue method', () => {
 		
-		xit('has an enqueue method', () => {
+		it('has an enqueue method', () => {
 			expect(typeof queue.enqueue).toBe('function')
 		})
 
-		xit('increases size when item is added', () => {
+		it('increases size when item is added', () => {
 			queue.enqueue('Snoopy')
 			expect(queue.size).toBe(1)
 		})
@@ -27,11 +27,11 @@ describe('Queue data structure', () => {
 
 	describe('dequeue method', () => {
 		
-		xit('has an dequeue method', () => {
+		it('has an dequeue method', () => {
 			expect(typeof queue.dequeue).toBe('function')
 		})
 
-		xit('decreases size when item is dequeued', () => {
+		it('decreases size when item is dequeued', () => {
 			queue.enqueue('Snoopy')
 			queue.enqueue('Woodstock')
 			queue.enqueue('Charlie Brown')
@@ -39,7 +39,7 @@ describe('Queue data structure', () => {
 			expect(queue.size).toBe(2)
 		})
 
-		xit('Dequeue returns items based on first in first out', () => {
+		it('Dequeue returns items based on first in first out', () => {
 			queue.enqueue('first')
 			queue.enqueue('second')
 			queue.enqueue('third')
